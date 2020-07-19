0a7c0629928663f67dbd33284bdcd4306cc3f438
Muhammad Niko <mn.muhammadniko@gmail.com>
2020-07-19 21:10:47 +0800

    Fix T_RiskLevels Seeder

--- database/seeds/RiskLevelsSeeder.php
+++ database/seeds/RiskLevelsSeeder.php
@@ -13,9 +13,9 @@ class RiskLevelsSeeder extends Seeder
     public function run()
     {
         DB::table('T_RiskLevels')->insert([
-            ['tingkat_risiko' => 'Renda','zona' => 'Zona C','color' => '#35fc03'],
-            ['tingkat_risiko' => 'Sedan','zona' => 'Zona B','color' => '#fcec03'],
-            ['tingkat_risiko' => 'Tingg','zona' => 'Zona A','color' => '#fc0303'],
+            ['tingkat_risiko' => 'Rendah','zona' => 'Zona C','color' => '#35fc03'],
+            ['tingkat_risiko' => 'Sedang','zona' => 'Zona B','color' => '#fcec03'],
+            ['tingkat_risiko' => 'Tinggi','zona' => 'Zona A','color' => '#fc0303'],
         ]);
     }
 }

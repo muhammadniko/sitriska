3970a32b1fc92d301d16f32f3815b99dbeaa627c
muhammadniko <mn.muhammadniko@gmail.com>
2023-02-13 21:52:14 +0800

    Update DBSeeder :arrow_up:

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserSeeder::class);
+        $this->call(UsersSeeder::class);
+        $this->call(RiskLevelsSeeder::class);
+        $this->call(LokasiSeeder::class);
+        $this->call(ScoreSeeder::class);
     }
 }
